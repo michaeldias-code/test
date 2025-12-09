14751507ec33c135dc1fbf3efc9657ccfed3cd13
michaeldias-code <michaelrpdias@gmail.com>
2025-12-09 11:22:16 -0300

    Update AI_Easy.js

--- chess-js/AI_Easy.js
+++ chess-js/AI_Easy.js
@@ -1,50 +1,17 @@
-// AI_Easy.js
-
 export class AI_Easy {
-
-    constructor(board, validator, enPassant) {
-        this.board = board;
-        this.validator = validator;
-        this.enPassant = enPassant;
-
+    constructor() {
         console.log("🤖 AI_Easy carregada (modo fácil).");
     }
 
     /**
-     * Retorna um movimento legal aleatório.
+     * Decide qual jogada usar entre todas as válidas
+     * @param {Array} validMoves
+     * @returns {object|null}
      */
-    findMove(color) {
-        const allMoves = this.getAllLegalMoves(color);
+    chooseMove(validMoves) {
+        if (!validMoves || validMoves.length === 0) return null;
 
-        if (allMoves.length === 0) {
-            return null;
-        }
-
-        const randomIndex = Math.floor(Math.random() * allMoves.length);
-        return allMoves[randomIndex];
-    }
-
-    /**
-     * Gera uma lista de todos os movimentos possíveis para a cor fornecida.
-     */
-    getAllLegalMoves(color) {
-        const moves = [];
-
-        for (let i = 0; i < 64; i++) {
-            const piece = this.board.board[i];
-
-            if (!piece || piece.cor !== color) continue;
-
-            const possibleMoves = this.validator.getPossibleMoves(i);
-
-            for (const dest of possibleMoves) {
-                moves.push({
-                    from: i,
-                    to: dest
-                });
-            }
-        }
-
-        return moves;
+        const randomIndex = Math.floor(Math.random() * validMoves.length);
+        return validMoves[randomIndex];
     }
 }
