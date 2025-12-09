6b546db16b23d5bfd89d8695ca697afdcfe98b9d
michaeldias-code <michaelrpdias@gmail.com>
2025-12-09 11:07:41 -0300

    Update AI_Medium.js

--- chess-js/AI_Medium.js
+++ chess-js/AI_Medium.js
@@ -1,24 +1,38 @@
-class AI_Medium {
+// AI_Medium.js
+
+export class AI_Medium {
+
     constructor(board, validator, enPassant) {
         this.board = board;
         this.validator = validator;
         this.enPassant = enPassant;
+
+        console.log("🤖 AI_Medium carregada (modo médio).");
     }
 
-    findMove(playerColor) {
+    findMove(color) {
+        const allMoves = this.getAllLegalMoves(color);
+
+        if (allMoves.length === 0) return null;
+
+        // Lógica igual ao Easy por enquanto
+        const idx = Math.floor(Math.random() * allMoves.length);
+        return allMoves[idx];
+    }
+
+    getAllLegalMoves(color) {
         const moves = [];
 
         for (let i = 0; i < 64; i++) {
             const p = this.board.board[i];
-            if (p && p.cor === playerColor) {
-                const possible = this.validator.getPossibleMoves(i);
-                for (let to of possible) moves.push({ from: i, to });
+            if (!p || p.cor !== color) continue;
+
+            const possible = this.validator.getPossibleMoves(i);
+            for (const d of possible) {
+                moves.push({ from: i, to: d });
             }
         }
 
-        if (moves.length === 0) return null;
-        return moves[Math.floor(Math.random() * moves.length)];
+        return moves;
     }
 }
-
-export { AI_Medium };
