ecfdf74ee05056cf6c0f0af582157670f751b63a
michaeldias-code <michaelrpdias@gmail.com>
2025-12-09 11:08:03 -0300

    Update AI_Hard.js

--- chess-js/AI_Hard.js
+++ chess-js/AI_Hard.js
@@ -1,24 +1,38 @@
-class AI_Hard {
+// AI_Hard.js
+
+export class AI_Hard {
+
     constructor(board, validator, enPassant) {
         this.board = board;
         this.validator = validator;
         this.enPassant = enPassant;
+
+        console.log("🤖 AI_Hard carregada (modo difícil).");
     }
 
-    findMove(playerColor) {
+    findMove(color) {
+        const allMoves = this.getAllLegalMoves(color);
+
+        if (allMoves.length === 0) return null;
+
+        // Em breve posso transformar isso num minimax...
+        const idx = Math.floor(Math.random() * allMoves.length);
+        return allMoves[idx];
+    }
+
+    getAllLegalMoves(color) {
         const moves = [];
 
         for (let i = 0; i < 64; i++) {
             const p = this.board.board[i];
-            if (p && p.cor === playerColor) {
-                const possible = this.validator.getPossibleMoves(i);
-                for (let to of possible) moves.push({ from: i, to });
+            if (!p || p.cor !== color) continue;
+
+            const possible = this.validator.getPossibleMoves(i);
+            for (const d of possible) {
+                moves.push({ from: i, to: d });
             }
         }
 
-        if (moves.length === 0) return null;
-        return moves[Math.floor(Math.random() * moves.length)];
+        return moves;
     }
 }
-
-export { AI_Hard };
