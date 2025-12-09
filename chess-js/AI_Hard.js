7a10f53931887363b4bdee0f7194f1084f31a87b
michaeldias-code <michaelrpdias@gmail.com>
2025-12-09 11:23:29 -0300

    Update AI_Hard.js

--- chess-js/AI_Hard.js
+++ chess-js/AI_Hard.js
@@ -1,38 +1,17 @@
-// AI_Hard.js
-
 export class AI_Hard {
-
-    constructor(board, validator, enPassant) {
-        this.board = board;
-        this.validator = validator;
-        this.enPassant = enPassant;
-
-        console.log("🤖 AI_Hard carregada (modo difícil).");
+    constructor() {
+        console.log("🤖 AI_Easy carregada (modo fácil).");
     }
 
-    findMove(color) {
-        const allMoves = this.getAllLegalMoves(color);
+    /**
+     * Decide qual jogada usar entre todas as válidas
+     * @param {Array} validMoves
+     * @returns {object|null}
+     */
+    chooseMove(validMoves) {
+        if (!validMoves || validMoves.length === 0) return null;
 
-        if (allMoves.length === 0) return null;
-
-        // Em breve posso transformar isso num minimax...
-        const idx = Math.floor(Math.random() * allMoves.length);
-        return allMoves[idx];
-    }
-
-    getAllLegalMoves(color) {
-        const moves = [];
-
-        for (let i = 0; i < 64; i++) {
-            const p = this.board.board[i];
-            if (!p || p.cor !== color) continue;
-
-            const possible = this.validator.getPossibleMoves(i);
-            for (const d of possible) {
-                moves.push({ from: i, to: d });
-            }
-        }
-
-        return moves;
+        const randomIndex = Math.floor(Math.random() * validMoves.length);
+        return validMoves[randomIndex];
     }
 }
